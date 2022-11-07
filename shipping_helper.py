0018059f0f0ff316e3df391c517fb12bceeaa137
DaisyLiu22 <sliu3@babson.edu>
2022-11-07 16:43:02 -0500

    Update

--- shipping_helper.py
+++ shipping_helper.py
@@ -4,7 +4,7 @@ def calculate_shipping(quantity):
     quantity = int(quantity)
     count = 0
     if quantity <= 4: 
-        if quantity = 0:
+        if quantity == 0:
             total_cost = 0   
         elif quantity > 0 and quantity <= 2:
             total_cost = 4000
@@ -13,7 +13,7 @@ def calculate_shipping(quantity):
     else:
         count = int(quantity / 4)
         extra_sets = quantity - count*4
-        if extra_sets = 0:
+        if extra_sets == 0:
             extra_cost = 0
         elif extra_sets > 0 and extra_sets <= 2:
             extra_cost = 4000
