4d0ffe9ff5f8bcf6ef32aa64d330d428bee19c48
DaisyLiu22 <sliu3@babson.edu>
2022-11-07 12:50:27 -0500

    update

--- shipping_helper.py
+++ shipping_helper.py
@@ -2,40 +2,26 @@ def calculate_shipping(quantity):
     """Calculate the total weight of the package,
     and returns the shipping fee"""
     quantity = int(quantity)
-    weight = 0.5
-    total_weight = float(quantity * weight)
     count = 0
-    if total_weight < 15:
-        if total_weight < 3:
-            total_cost = 3000
-        elif total_weight >= 3 and total_weight < 5:
+    if quantity <= 4:
+        if quantity <= 2:
             total_cost = 4000
-        elif total_weight >= 5 and total_weight < 7:
-            total_cost = 5000
-        elif total_weight >= 7 and total_weight < 10:
+        else:
             total_cost = 6000
-        else:
-            total_cost = 7000
     else:
-        count = int(total_weight / 15)
-        extra_weight = total_weight - count*15
-        if extra_weight < 3:
-            extra_cost = 3000
-        elif extra_weight >= 3 and extra_weight < 5:
+        count = int(quantity / 4)
+        extra_sets = quantity - count*4
+        if extra_sets <= 2:
             extra_cost = 4000
-        elif extra_weight >= 5 and extra_weight < 7:
-            extra_cost = 5000
-        elif extra_weight >= 7 and extra_weight < 10:
-            extra_cost = 6000
         else:
-            extra_cost = count*7000
-        total_cost = float(count*7000 + extra_cost)
+            extra_cost = 6000
+        total_cost = float(count*6000 + extra_cost)
     
     return total_cost
 
 def total_amount(quantity):
     quantity = int(quantity)
-    price = 23000
+    price = 54000
     total_deposit = calculate_shipping(quantity) + quantity * price
     return total_deposit
         
@@ -43,7 +29,7 @@ def main():
     """
     You can test all the functions here
     """
-    # calculate_shipping(20)
+    calculate_shipping(20)
 
 
 if __name__ == '__main__':
